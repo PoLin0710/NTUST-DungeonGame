3fd08dfe7f0dfd82cafbc9924418901204da5c9f
yehyi817 <yehyi817@gmail.com>
2024-05-03 23:23:51 +0800

    修改NONE的名稱

--- src/Equipment.h
+++ src/Equipment.h
@@ -4,7 +4,7 @@ using namespace std;
 
 enum WEAPON_IDX
 {
-	NONE = 0,
+	WEAPON_NONE = 0,
 	WOODEN_SWORD = 1,
 	HAMMER = 2,
 	GIANT_HAMMER = 3,
@@ -14,7 +14,7 @@ enum WEAPON_IDX
 
 enum ARMOR_IDX
 {
-	NONE = 0,
+	ARMOR_NONE = 0,
 	WOODEN_SHIELD = 1,
 	PLATE_ARMOR = 2,
 	LEATHER_ARMOR = 3,
@@ -22,9 +22,9 @@ enum ARMOR_IDX
 	LAUREL_WREATH = 5
 };
 
-enum ACCESSORYIDX
+enum ACCESSORY_IDX
 {
-	NONE = 0,
+	ACCESSORY_NONE = 0,
 	HOLY_GRAIL = 1,
 	SHOES = 2,
 	BRACELET = 3
@@ -32,9 +32,9 @@ enum ACCESSORYIDX
 
 class Equipment
 {
-	int weapon = WEAPON_IDX::NONE;
-	int armor = ARMOR_IDX::NONE;
-	int accessory = ACCESSORYIDX::NONE;
+	int weapon = WEAPON_IDX::WEAPON_NONE;
+	int armor = ARMOR_IDX::ARMOR_NONE;
+	int accessory = ACCESSORY_IDX::ACCESSORY_NONE;
 	int weaponAtk = 0;
 	int armorDef = 0;
 
