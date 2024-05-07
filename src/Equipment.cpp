fac02b243d1f3855daa0a5193ee18ef3bde1f159
yehyi817 <yehyi817@gmail.com>
2024-05-07 13:14:31 +0800

    更新排序順序 ps.吃飯不要寫code

--- src/Equipment.cpp
+++ src/Equipment.cpp
@@ -174,7 +174,7 @@ skill Equipment::getSkill(int idx) const
 
 bool compareSkill(const skill& s1, const skill& s2)
 {
-	return s1.skillIdx > s2.skillIdx;
+	return s1.skillIdx < s2.skillIdx;
 }
 
 bool equalSkill(const skill& s1, const skill& s2)
