f92157b99dd93e84d14d289770d99a4d66de5586
PoLin <oliver590617@gmail.com>
2024-05-09 17:42:13 +0800

    新增item架構

---
+++ src/Item.h
@@ -0,0 +1,11 @@
+#ifndef _ITEM_H_
+#define _ITEM_H_
+
+enum ITEM_IDX {
+	GODSBEARD = 0,
+	GOLDENROOT = 1,
+	TELEPORTSCROLL = 2,
+	TENT = 3
+};
+
+#endif // _ITEM_H_
--- src/List.h
+++ src/List.h
@@ -6,17 +6,20 @@
 #include <string>
 #include "Buff.h"
 #include "Equipment.h"
+#include "Item.h"
 
 extern std::map<int, std::string> weaponName;
 extern std::map<int, std::string> armorName;
 extern std::map<int, std::string> accessoryName;
 extern std::map<int, std::string> skillName;
 extern std::map<int, std::string> buffName;
+extern std::map<int, std::string> ItemName;
 
 std::string getWeaponName(int idx);
 std::string getArmorName(int idx);
 std::string getAccessoryName(int idx);
 std::string getSkillName(int idx);
 std::string getBuffName(int idx);
+std::string getItemName(int idx);
 
 #endif // _LIST_H_
