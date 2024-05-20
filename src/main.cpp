e3b27f5d964c4aea705b6954318fe3d4aea0c7b1
PoLin <oliver590617@gmail.com>
2024-05-20 20:33:31 +0800

    upload commit for main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,27 @@
-﻿#include "Game.h"
+﻿/***********************************************************************
+ * File: main.cpp
+ * Author:
+ *        B11215040 HUANG,PO-LIN (oliver590617@gmail.com)
+ *        B11215030 SONG,BING-YU (yehyi817@gmail.com)
+ *        B11215014 王鈺晴 (sunnyching901105@gmail.com)
+ *        B11215008 洪伯穎 (n590762743@gmail.com)
+ * Create Date: 2024-05-20
+ * Editor:
+ *        B11215040 HUANG,PO-LIN (oliver590617@gmail.com)
+ *        B11215030 SONG,BING-YU (yehyi817@gmail.com)
+ *        B11215014 王鈺晴 (sunnyching901105@gmail.com)
+ *        B11215008 洪伯穎 (n590762743@gmail.com)
+ * Update Date: 2024-05-20
+ * Description: Main
+***********************************************************************/
+
+#include "Game.h"
 
 int main()
 {
+	// Declare
 	Game game;
+
+	// Game start
 	game.start();
-
-	//Role* man1 = new Role("Role1", '1')/*, * man2 = new Role("Role2"), * man3 = new Role(("Role3"))*/;
-	/*Entity* man4 = new Entity("Player4", ENTITY_TYPE::ENEMY), * man5 = new Entity("Player5", ENTITY_TYPE::ENEMY), * man6 = new Entity("Player6", ENTITY_TYPE::ENEMY);
-	std::vector<Entity*> Ent = { man4, man5,man6 };
-	std::vector<Role*> rol = { man1, man2,man3 };
-
-
-	man2->update();
-	man3->update();
-	man4->update();
-	man5->update();
-	man6->update();*/
-	////Combat combat(rol, Ent);
-	////combat.start();
-	//Shop shop;
-
-	//man1->update();
-	//man1->printInfo(0, 0);
-	////man1->printBag();
-	//man1->unWearEquipment(UNWEAR_WEAPON);
-
-	//man1->printInfo(0, 10);
-	////std::cout << "Money: " << man1->getMoney();
-	//man1->insertBag(shop.intoShop(man1->getMoney()));
-	////std::cout << "Money: " << man1->getMoney() << std::endl;
-	////man1->printInfo();
-	//man1->printBag();
-	////system("pause");
-
-	////man4->useSkill(0, Ent, rol);
-	////man1->useItem(ITEM_IDX::IGODSBEARD, 13);
-	////man1->printInfo();
-	////man1->printBag();
-	////man1->useSkill(0, rol, Ent);
-	////man1->useSkill(0, rol, Ent);
-	////man4->printInfo();
-	////std::cout << "\n";
-	////int useidx;
-	////std::cout << "Choose skills: ";
-	////std::cin >> useidx;
-	////man1->useSkill(useidx, rol, Ent);
-	////man2->printInfo();
-	////std::cout << "\n\n";
-	////std::cout << "Choose skills: ";
-	////std::cin >> useidx;
-	////man2->useSkill(useidx, Ent, rol);
-
-	////for (auto i : Ent)
-	////{
-	////	i->printInfo();
-	////}
-
-	////for (auto i : rol)
-	////{
-	////	i->printInfo();
-	////}
-
 }
