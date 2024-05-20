e6366485b5d457007b8def4c280cbe12c01f1d9e
PoLin <oliver590617@gmail.com>
2024-05-20 21:15:36 +0800

    update commit for list

--- src/List.cpp
+++ src/List.cpp
@@ -1,5 +1,24 @@
+/***********************************************************************
+ * File: List.cpp
+ * Author:
+ *        B11215040 HUANG,PO-LIN (oliver590617@gmail.com)
+ *        B11215030 SONG,BING-YU (yehyi817@gmail.com)
+ *        B11215014 ¤ýà±´¸ (sunnyching901105@gmail.com)
+ *        B11215008 ¬x§B¿o (n590762743@gmail.com)
+ * Create Date: 2024-05-20
+ * Editor:
+ *        B11215040 HUANG,PO-LIN (oliver590617@gmail.com)
+ *        B11215030 SONG,BING-YU (yehyi817@gmail.com)
+ *        B11215014 ¤ýà±´¸ (sunnyching901105@gmail.com)
+ *        B11215008 ¬x§B¿o (n590762743@gmail.com)
+ * Update Date: 2024-05-20
+ * Description: This file implements functions to retrieve names associated with various game items,
+ *				such as weapons, armor, accessories, skills, buffs, and general items.
+***********************************************************************/
 #include "List.h"
 
+
+// ==========================Initialize maps storing item names==========================
 std::map<int, std::string> weaponName = {
 	{WEAPON_IDX::WEAPON_NONE, "NONE"},
 	{WEAPON_IDX::WOODEN_SWORD, "WOODEN_SWORD"},
@@ -68,53 +87,108 @@ std::map<int, string> ItemName = {
 		{ITEM_IDX::IElucidator_Dark_Repulser, "ELUCIDATOR_REPULSER"}
 };
 
+// ======================================END======================================
+
+/**
+ * Intent: Retrieve the name of a weapon based on its index.
+ * Pre: The weapon index must be valid.
+ * Post: Returns the name of the weapon if found; otherwise, returns "UNKNOWN_WEAPON".
+ * \param idx The index of the weapon.
+ * \return The name of the weapon corresponding to the provided index.
+ */
 std::string getWeaponName(int idx)
 {
+	//Find
 	if (weaponName.find(idx) != weaponName.end())
 	{
 		return weaponName[idx];
 	}
+
 	return "UNKNOWN_WEAPON";
 }
 
+/**
+ * Intent: Retrieve the name of an armor based on its index.
+ * Pre: The armor index must be valid.
+ * Post: Returns the name of the armor if found; otherwise, returns "UNKNOWN_ARMOR".
+ * \param idx The index of the armor.
+ * \return The name of the armor corresponding to the provided index.
+ */
 std::string getArmorName(int idx)
 {
+	//Find
 	if (armorName.find(idx) != armorName.end())
 	{
 		return armorName[idx];
 	}
+
 	return "UNKNOWN_ARMOR";
 }
 
+/**
+ * Intent: Retrieve the name of an accessory based on its index.
+ * Pre: The accessory index must be valid.
+ * Post: Returns the name of the accessory if found; otherwise, returns "UNKNOWN_ACCESSORY".
+ * \param idx The index of the accessory.
+ * \return The name of the accessory corresponding to the provided index.
+ */
 std::string getAccessoryName(int idx)
 {
+	//Find
 	if (accessoryName.find(idx) != accessoryName.end())
 	{
 		return accessoryName[idx];
 	}
+
 	return "UNKNOWN_ACCESSORY";
 }
 
+/**
+ * Intent: Retrieve the name of a skill based on its index.
+ * Pre: The skill index must be valid.
+ * Post: Returns the name of the skill if found; otherwise, returns "UNKNOWN_SKILL".
+ * \param idx The index of the skill.
+ * \return The name of the skill corresponding to the provided index.
+ */
 std::string getSkillName(int idx)
 {
+	//Find
 	if (skillName.find(idx) != skillName.end())
 	{
 		return skillName[idx];
 	}
+
 	return "UNKNOWN_SKILL";
 }
 
+/**
+ * Intent: Retrieve the name of a buff based on its index.
+ * Pre: The buff index must be valid.
+ * Post: Returns the name of the buff if found; otherwise, returns "UNKNOWN_BUFF".
+ * \param idx The index of the buff.
+ * \return The name of the buff corresponding to the provided index.
+ */
 std::string getBuffName(int idx)
 {
+	//Find
 	if (buffName.find(idx) != buffName.end())
 	{
 		return buffName[idx];
 	}
+
 	return "UNKNOWN_BUFF";
 }
 
+/**
+ * Intent: Retrieve the name of an item based on its index.
+ * Pre: The item index must be valid.
+ * Post: Returns the name of the item if found; otherwise, returns "UNKNOWN_ITEM".
+ * \param idx The index of the item.
+ * \return The name of the item corresponding to the provided index.
+ */
 std::string getItemName(int idx)
 {
+	//Find
 	if (ItemName.find(idx) != ItemName.end())
 	{
 		return ItemName[idx];
--- src/List.h
+++ src/List.h
@@ -1,3 +1,20 @@
+/***********************************************************************
+ * File: List.h
+ * Author:
+ *        B11215040 HUANG,PO-LIN (oliver590617@gmail.com)
+ *        B11215030 SONG,BING-YU (yehyi817@gmail.com)
+ *        B11215014 ¤ýà±´¸ (sunnyching901105@gmail.com)
+ *        B11215008 ¬x§B¿o (n590762743@gmail.com)
+ * Create Date: 2024-05-20
+ * Editor:
+ *        B11215040 HUANG,PO-LIN (oliver590617@gmail.com)
+ *        B11215030 SONG,BING-YU (yehyi817@gmail.com)
+ *        B11215014 ¤ýà±´¸ (sunnyching901105@gmail.com)
+ *        B11215008 ¬x§B¿o (n590762743@gmail.com)
+ * Update Date: 2024-05-20
+ * Description: This header file defines functions to retrieve names associated with various game items,
+ *				such as weapons, armor, accessories, skills, buffs, and general items.
+***********************************************************************/
 #ifndef _LIST_H_
 #define _LIST_H_
 
@@ -8,6 +25,7 @@
 #include "Equipment.h"
 #include "Shop.h"
 
+// Declare Map for Type index to English Name(string)
 extern std::map<int, std::string> weaponName;
 extern std::map<int, std::string> armorName;
 extern std::map<int, std::string> accessoryName;
@@ -15,6 +33,7 @@ extern std::map<int, std::string> skillName;
 extern std::map<int, std::string> buffName;
 extern std::map<int, std::string> ItemName;
 
+// Declare get Name function
 std::string getWeaponName(int idx);
 std::string getArmorName(int idx);
 std::string getAccessoryName(int idx);
